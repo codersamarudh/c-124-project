42d4edd6b4e228ef5c0ad322be64af0578568da7
codersamarudh <93390477+codersamarudh@users.noreply.github.com>
2022-09-29 15:50:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+function setup() {
+    video=createCapture(VIDEO);
+    video.size(550,500);
+canvas=createCanvas(550,500);
+canvas.position(560,150);
+poseNet=ml5.poseNet(video,modelLoaded);
+poseNet.on('pose',gotPoses);
+}
+
+function draw(){
+    background('#FFA500');
+
+}
+
+function modelLoaded() {
+    console.log('poseNet is initialized');
+}
+
+function gotPoses(results){
+    if(results.length >0){
+        console.log(results);
+    }
+
+
+}
+
+
+
+
+
+
+
